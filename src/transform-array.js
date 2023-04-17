09fb449b1f898f7e387e1c31cf30245a07ed89a8
Almas Kilichov <itsmywaytoamerica@gmail.com>
2023-04-17 23:10:18 +0600

    test

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let newArr = arr;
+  let controlSequence = '';
+  let controlSequences = [
+    '--discard-next',
+    '--discard-prev',
+    '--double-next',
+    '--double-prev'
+  ]
+
+  for (let i = 0; i < arr.length; i++) {
+    if (controlSequences.includes(arr[i])) {
+      controlSequence = arr[i]
+      if (controlSequence == controlSequences[0]) {
+        newArr.splice(i, i+1);
+      } else if (controlSequence == controlSequences[1]) {
+        if (i != 0) {
+          newArr.splice(i-1, i);
+        } else {
+          newArr.splice(i, i)
+        }
+       
+      } else if (controlSequence == controlSequences[2]) {
+        newArr[i] = newArr[i+1];
+      } else if (controlSequence == controlSequences[3]) {
+        if (i != 0) {
+          newArr[i] = newArr[i-1];
+        } else {
+          newArr.splice(i, i)
+        }
+        
+      }
+    }
+  }
+
+  return newArr;
 }
 
 module.exports = {
